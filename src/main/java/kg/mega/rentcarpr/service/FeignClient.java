9d24708f85dc851759bb2055733574d56ba1411e
Bektursun_K <beka.turganbaev98@gmail.com>
2024-01-18 12:09:00 +0300

    Создан Microservice sendMail &pom && properies проекта

---
+++ src/main/java/kg/mega/rentcarpr/service/FeignClient.java
@@ -0,0 +1,10 @@
+package kg.mega.rentcarpr.service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+@org.springframework.cloud.openfeign.FeignClient(name = "mailSender",url = "http://localhost:8080/api/v1/mail")
+
+public interface FeignClient {
+    @PostMapping("sendMail")
+    void sendMail(@RequestParam String to, @RequestParam String subject, @RequestParam String text) ;
+
+}
